9c4f80829d4d15c3bfc126bfa47bb7493ff7c66d
Kevin Gosse <krix33@gmail.com>
2025-06-17 10:28:47 +0200

    WIP

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -33,6 +33,9 @@ Logs.AssertContains(logs, "AppDomainCreationFinished - DefaultDomain - HResult S
 // Clear the logs before the next tests
 Logs.Clear();
 
+RejitTest.Test();
+return;
+
 var tests = new List<ITest>
 {
     new AssemblyLoadContextTests(),
---
+++ src/TestApp/RejitTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TestApp;
+internal class RejitTest
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Test()
+    {
+        Console.WriteLine("RejitTest");
+    }
+}
