7ae72b4221129ff168a50c96b8131a7903ab409a
Kevin Gosse <krix33@gmail.com>
2025-06-21 21:45:38 +0200

    Set LD_LIBRARY_PATH

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ set -e
 
 PROFILER_DLL=$(realpath ./src/ManagedDotnetProfiler/bin/Release/net9.0/linux-x64/native/ManagedDotnetProfiler.so)
 
+export LD_LIBRARY_PATH=$(realpath ./src/ManagedDotnetProfiler/bin/Release/net9.0/linux-x64/native/):$LD_LIBRARY_PATH
+
 export CORECLR_ENABLE_PROFILING=1
 export CORECLR_PROFILER="{0A96F866-D763-4099-8E4E-ED1801BE9FBC}"
 export CORECLR_PROFILER_PATH="$PROFILER_DLL"
