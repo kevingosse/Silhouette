55ed72c90f611573cef7302d379d0e7af91faf6c
Kevin Gosse <krix33@gmail.com>
2025-06-28 16:03:43 +0200

    Scope environment variables

--- test.ps1
+++ test.ps1
@@ -2,13 +2,18 @@ $ErrorActionPreference = "Stop"
 
 $profilerDll = Resolve-Path ./src/ManagedDotnetProfiler/bin/Release/net9.0/win-x64/native/ManagedDotnetProfiler.dll
 
-$env:CORECLR_ENABLE_PROFILING=1
-$env:CORECLR_PROFILER="{0A96F866-D763-4099-8E4E-ED1801BE9FBC}"
-$env:CORECLR_PROFILER_PATH=$profilerDll
+$envVars = @{
+    'CORECLR_ENABLE_PROFILING' = '1'
+    'CORECLR_PROFILER' = '{0A96F866-D763-4099-8E4E-ED1801BE9FBC}'
+    'CORECLR_PROFILER_PATH' = "$profilerDll"
+}
 
 Write-Host "Running TestApp with profiler..."
-./src/TestApp/bin/Release/net9.0/TestApp.exe
-$exitCode = $LASTEXITCODE
+
+$p = Start-Process -FilePath ./src/TestApp/bin/Release/net9.0/TestApp.exe `
+    -NoNewWindow -Wait -PassThru -Environment $envVars
+
+$exitCode = $p.ExitCode
 
 if ($exitCode -ne 0) {
     Write-Error "TestApp failed with exit code $exitCode"
